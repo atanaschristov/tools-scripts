2f31459caf409b6dcf68be77b2d29ebca1bcb1f3
Atanas Christov <atanaschristov@gmail.com>
2016-09-03 18:54:51 +0200

    whereiamfrom is a script which uses SSH_CLIENT var to get the remote ip address of the ssh session, which is then used to get its geolocation by using freegeoip.net service

---
+++ whereiamfrom.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ip=`echo $SSH_CLIENT | awk '{print $1}'`
+echo "  IP: $ip";
+name=`host $ip | grep -oE "[^' ']+$"`;
+echo "NAME: $name";
+IFS=',' read -ra geo <<< `curl -s --socks5 127.0.0.1:9050 freegeoip.net/csv/$ip`
+geo=${geo[@]:1}
+echo "FROM: ${geo}"
+# echo "FROM: ${geo[2]}(${geo[1]}) ${geo[4]}(${geo[3]}) ${geo[5]} ${geo[6]} ${geo[7]} ${geo[8]} ${geo[9]} ${geo[10]}" 
+ 
+
