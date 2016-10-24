141258cc2072011ee1f08274d082e3891ac35675
Atanas Christov <atanaschristov@gmail.com>
2016-10-24 15:01:07 +0200

    notify the expiration of the account 40 hours before

--- whenxShellzExpire.sh
+++ whenxShellzExpire.sh
@@ -6,6 +6,9 @@ if [ ! -z "$status" ]; then
 fi
 
 hours=`curl -s https://www.xshellz.com/api/plain/downtempo/shellexpires`
+# FIND A WAY TO USE PROXY WITHOUT BOT VERIFICATION 
+# hours=`curl --socks5 localhost:9050 -s https://www.xshellz.com/api/plain/downtempo/shellexpires`
+
 echo $hours;
 
 export DISPLAY=:0 # on which display the notification and alarm will be displayed
@@ -14,7 +17,7 @@ eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome
 
 DISPLAY=:0.0 /usr/bin/notify-send 'NOTE!!' "Checking when xShellz expires ($hours hours left)" --icon=dialog-information
 
-if [ $hours -le 10 ]; then
+if [ $hours -le 40 ]; then
 	killall notify-osd;
 	/usr/bin/notify-send 'WARNING!!' "xShellz will expire in $hours hours." --icon=dialog-information;
 	/home/subnetcat/apps/bin/notify.py 'WARNING!!' "xShellz will expire in $hours hours.\n Come to our irc.freenode.net #xShellz channel\nand type: !keep downtempo (Your shell will be extended for 336 hours more)"
